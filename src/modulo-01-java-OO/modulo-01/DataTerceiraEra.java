511eb8423a0f9307605e0d5c3b0207339c6bd165
Unknown <mateusbpt1@gmail.com>
2016-09-28 20:31:35 -0300

    feat(DataTerceiraEra): adicionar método ehBissexto() e testes.

--- src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -4,6 +4,12 @@ public class DataTerceiraEra {
     private int mes;
     private int ano;
 
+    public DataTerceiraEra (int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
     public int getDia() {
         return dia;
     }    
@@ -16,4 +22,11 @@ public class DataTerceiraEra {
         return ano;
     }    
 
+    public boolean ehBissexto(){
+
+        if(ano%400 == 0 || (ano%100 != 0 && ano%4 == 0)){ //Condições para ocorrer ano Bissexto
+            return true;   
+        }
+        return false;
+    }
 }
