e8c88481b006a5df5f0332ecd3c50b30b8f5953d
Unknown <mateusbpt1@gmail.com>
2016-09-28 16:12:22 -0300

    feat(CestoDeLembas): adicionar testes para classe.

--- src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -1,14 +1,13 @@
 public class CestoDeLembas {
 
     private int quantidadeLembas;
-    private boolean divisaoLembas;
 
     public CestoDeLembas (int quantidadeLembas) {
         this.quantidadeLembas = quantidadeLembas;
     }
 
     public boolean podeDividirEmPares() {
-        if(quantidadeLembas >= 2 && quantidadeLembas <= 100) { 
+        if(quantidadeLembas > 2 && quantidadeLembas <= 100) { 
             if(quantidadeLembas%2 == 0) {
                 return true;   
             }
