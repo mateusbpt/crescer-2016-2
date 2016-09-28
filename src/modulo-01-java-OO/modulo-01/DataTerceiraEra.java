80ed104bad3cb0320558c910794a0010b508455b
Unknown <mateusbpt1@gmail.com>
2016-09-28 20:08:18 -0300

    feat(DataTerceiraEra): adicionar classe DataTerceiraEra com seus atributos e métodos get.

---
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -0,0 +1,19 @@
+public class DataTerceiraEra {
+
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public int getDia() {
+        return dia;
+    }    
+
+    public int getMes() {
+        return mes;
+    }    
+
+    public int getAno() {
+        return ano;
+    }    
+
+}
