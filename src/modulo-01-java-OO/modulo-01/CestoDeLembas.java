438eab48b4b1f3a59890c8cf57a907178061f18e
Unknown <mateusbpt1@gmail.com>
2016-09-28 00:54:10 -0300

    feat(CestoDeLembas): adicionar classe CestoDeLembas e seus atributos.

---
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -0,0 +1,17 @@
+public class CestoDeLembas {
+
+    private int quantidadeLembas;
+    private boolean divisaoLembas;
+    
+    public CestoDeLembas (int quantidadeLembas){
+        this.quantidadeLembas = quantidadeLembas;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}    
