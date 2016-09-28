b52c28eda0664adc89128fe3be089ce779239128
Unknown <mateusbpt1@gmail.com>
2016-09-28 01:41:37 -0300

    fix(ElfoTest, CestoDeLembas, Item): ajustar identação do código.

--- src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -2,17 +2,17 @@ public class CestoDeLembas {
 
     private int quantidadeLembas;
     private boolean divisaoLembas;
-    
+
     public CestoDeLembas (int quantidadeLembas) {
         this.quantidadeLembas = quantidadeLembas;
     }
-    
+
     public boolean podeDividirEmPares() {
         if(quantidadeLembas >= 2 && quantidadeLembas <= 100) { 
             if(quantidadeLembas%2 == 0) {
-             return true;   
+                return true;   
             }
         }
         return false;
-        }    
+    }    
 }
--- src/modulo-01-java-OO/modulo-01/ElfoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoTest.java
@@ -31,42 +31,42 @@ public class ElfoTest
         //Assert
         assertEquals(42, elfoTeste.getFlecha().getQuantidade()); 
     }  
-    
+
     @Test
     public void elfoAtiraUmaFlecha() {
-       //Act
-       Elfo elfoTeste1 = new Elfo ("Legolas");
-       elfoTeste1.atirarFlecha();
-       //Assert
-       assertEquals(41, elfoTeste1.getFlecha().getQuantidade()); 
+        //Act
+        Elfo elfoTeste1 = new Elfo ("Legolas");
+        elfoTeste1.atirarFlecha();
+        //Assert
+        assertEquals(41, elfoTeste1.getFlecha().getQuantidade()); 
     } 
-    
+
     @Test
     public void elfoAtiraDezFlechas() {
         //Act
         Elfo elfoTeste2 = new Elfo ("Arwen");
         int i = 10;
         int num = 42;
-            while(i != 0){
-                elfoTeste2.atirarFlecha();
-                i--;
-                num--;
-            }
+        while(i != 0){
+            elfoTeste2.atirarFlecha();
+            i--;
+            num--;
+        }
         //Assert
         assertEquals(num, elfoTeste2.getFlecha().getQuantidade()); 
     } 
-    
+
     @Test
     public void elfoAtiraTodasAsFlechas() {
         //Act
         Elfo elfoTeste3 = new Elfo ("Galadriel");
         int i = elfoTeste3.getFlecha().getQuantidade();
-            while(i != 0){
-                elfoTeste3.atirarFlecha();
-                i--;
-            }
+        while(i != 0){
+            elfoTeste3.atirarFlecha();
+            i--;
+        }
         //Assert
         assertEquals(0, i); 
     } 
-    
+
 }
