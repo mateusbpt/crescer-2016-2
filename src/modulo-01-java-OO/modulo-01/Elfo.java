e298ba8298706d73565ea7741dc5beeb211a8760
Unknown <mateusbpt1@gmail.com>
2016-09-28 01:42:14 -0300

    feat(Elfo): adicionar método atacarDwarves().

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -3,11 +3,13 @@ public class Elfo {
     private Item arco;
     private Item flecha;
     private int experiencia;
+    private int dwarves;
 
     public Elfo(String n) {
         nome = n;
         arco = new Item("Arco", 1);
         flecha = new Item("Flechas", 2);
+        dwarves = 110;
     }
 
     public void setNome(String n) {
@@ -17,25 +19,31 @@ public class Elfo {
     public String getNome() {
         return nome;
     }
-    
+
     public Item getArco() {
         return arco;
     }
-    
+
     public Item getFlecha() {
         return flecha;
     }
-    
+
     public void atirarFlecha() {
         if(flecha.getQuantidade() > 0){ //garante que o elfo não possua flechas negativas
-        flecha.setQuantidade(flecha.getQuantidade() - 1);
-        experiencia++;
-       } 
+            flecha.setQuantidade(flecha.getQuantidade() - 1);
+            experiencia++;
+        } 
+    }
+
+    public void atacarDwarves() {
+        atirarFlecha();
+        if(dwarves > 0 && flecha.getQuantidade() > 0){
+            dwarves -= 10;
+        }
     }
     /*public void atirarFlechaRefactory() {
-        experiencia++;
-        flecha.setQuantidade(flecha.getQuantidade()-1);
+    experiencia++;
+    flecha.setQuantidade(flecha.getQuantidade()-1);
     }*/
 }
-
 
