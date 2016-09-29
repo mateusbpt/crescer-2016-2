3392a48023666197edabfbb11aeefae560298757
Unknown <mateusbpt1@gmail.com>
2016-09-28 21:25:37 -0300

    feat(Dwarf): adicionar método getNumeroSorte() e testes.

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -3,7 +3,11 @@ public class Dwarf {
     private int vida;
     private String nome;
     private DataTerceiraEra dataNascimento;
-    
+
+    public Dwarf(){ //para compilar a ElfoTest
+        this.vida = 110;
+    }
+
     public Dwarf(String nome, DataTerceiraEra dataNascimento) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
@@ -17,6 +21,17 @@ public class Dwarf {
     public int getVida(){
         return vida;
     }
-}
 
-    
+    public double getNumeroSorte(){
+        boolean vidaDwarf = this.vida >= 80 && this.vida <= 90;
+        boolean nomeDwarf = this.nome.equals("Seixas") || this.nome.equals("Meireles");
+        double numero = 101.0; 
+        if(dataNascimento.ehBissexto() == true && vidaDwarf == true){
+            return numero*(-33.0);    
+        }
+        if(dataNascimento.ehBissexto() == false && nomeDwarf == true){
+            return (numero*(33.0))%100;    
+        }
+        return numero;
+    }
+}
--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -7,20 +7,23 @@ public class DwarfTest {
 
     @Test
     public void dwarfNasce110Vida(){
-        Dwarf dwarf = new Dwarf();
-        assertEquals(110, dwarf.getVida());
+        DataTerceiraEra data = new DataTerceiraEra (25, 12, 2000);
+        Dwarf dwarf1 = new Dwarf("Thorin", data);
+        assertEquals(110, dwarf1.getVida());
     }
 
     @Test
     public void dwarfPerdeVidaUmaVez(){
-        Dwarf dwarf = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra (25, 12, 2000);
+        Dwarf dwarf = new Dwarf("Thorin", data);
         dwarf.perderVida();
         assertEquals(100, dwarf.getVida());
     }
 
     @Test
     public void dwarfPerdeVidaDezVezes(){
-        Dwarf dwarf = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra (25, 12, 2000);
+        Dwarf dwarf = new Dwarf("Thorin", data);
         int ataqueDwarf = 10;
         while(ataqueDwarf != 0){
             dwarf.perderVida();
@@ -29,4 +32,36 @@ public class DwarfTest {
         assertEquals(10, dwarf.getVida());
     }
 
+    @Test
+    public void numeroSorteDwarfBissexto(){
+        //Arrange
+        DataTerceiraEra data = new DataTerceiraEra (25, 12, 2000);
+        Dwarf dwarf1 = new Dwarf("Thorin", data);
+
+        //Act
+        dwarf1.perderVida();
+        dwarf1.perderVida();
+
+        //Assert
+        assertEquals(-3333, (int)dwarf1.getNumeroSorte()); //para assertEquals double era necessário 3 parâmetros
+    }
+
+    @Test
+    public void numeroSorteDwarfSeixas(){
+        //Arrange
+        DataTerceiraEra data = new DataTerceiraEra (12, 01, 1997);
+        Dwarf dwarf1 = new Dwarf("Seixas", data);
+        //Act & Assert
+        assertEquals(33, (int)dwarf1.getNumeroSorte()); //para assertEquals double era necessário 3 parâmetros
+    }
+
+    @Test
+    public void numeroSorteDwarf(){
+        //Arrange
+        DataTerceiraEra data = new DataTerceiraEra (12, 01, 2000);
+        Dwarf dwarf1 = new Dwarf("Seixas", data);
+        //Act & Assert
+        assertEquals(101, (int)dwarf1.getNumeroSorte()); //para assertEquals double era necessário 3 parâmetros
+    }
 }
+
