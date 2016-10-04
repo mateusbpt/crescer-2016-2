62fff298be4c1721572236434124d17eb3431dde
Unknown <mateusbpt1@gmail.com>
2016-10-04 14:48:45 -0300

    to-do(ElfoVerde): adicionar comentário para método que precisa ser melhorado.

--- src/modulo-01-java-OO/modulo-01/ElfoVerde.java
+++ src/modulo-01-java-OO/modulo-01/ElfoVerde.java
@@ -8,8 +8,8 @@ public class ElfoVerde extends Elfo {
         super(nome,quantidadeFlechas);      
     }
 
-    public void ganharItem(Item item) {
-        boolean itensPossiveis = "Espada de aço valiriano".equals(item.getDescricao()) || "Arco e Flecha de Vidro".equals(item.getDescricao());
+    public void ganharItem(Item item) { //TO-DO: ajustar para deixar mais flexível
+        boolean itensPossiveis = item != null &&("Espada de aço valiriano".equals(item.getDescricao()) || "Arco e Flecha de Vidro".equals(item.getDescricao()));
         if(itensPossiveis){
             inventario.adicionarItem(item);
         }
