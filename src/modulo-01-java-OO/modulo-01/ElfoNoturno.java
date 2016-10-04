aab14c206a56a954836a4fd0ff8775ea551feb40
Unknown <mateusbpt1@gmail.com>
2016-10-04 10:59:02 -0300

    feat(ElfoNoturno): adicionar classe ElfoNoturno e ajustar métodos que terão mudanças dos métodos da classe pai.

---
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
@@ -0,0 +1,24 @@
+public class ElfoNoturno extends Elfo{
+
+    public ElfoNoturno(String nome){
+        super(nome);
+    }
+
+    public ElfoNoturno(String nome, int quantidadeFlechas){
+        super(nome,quantidadeFlechas);      
+    }
+
+    /*5.2 Sem o ajuste do item 5.3 não é possivel chegar a vida a 0, pois o double vai dividir muitas vezes e 
+    sempre aumentará a casas de 0, mas nunca chegará a 0.0. Exemplo: 0.0000001 => 000000000.1  */
+    public void atirarFlecha(Dwarf dwarf) {
+        int quantidadeFlechas = getFlecha().getQuantidade();
+        if(quantidadeFlechas > 0){ //garante que o elfo não possua flechas negativas
+            getFlecha().setQuantidade(quantidadeFlechas - 1);   
+            dwarf.perderVida(); 
+            experiencia+=3;
+            vida-=(vida*0.05);
+            status = vida > 1.0 ? Status.VIVO : Status.MORTO;
+        }
+    }
+
+}    
