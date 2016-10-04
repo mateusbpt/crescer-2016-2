3cf02420c17210ab38ddb14d4e18b7e2255a3506
Unknown <mateusbpt1@gmail.com>
2016-10-04 13:37:48 -0300

    refactor(ElfoNoturno): tirar duplicação com ajuste no método atirarFlecha() da classe pai e testes.

--- src/modulo-01-java-OO/modulo-01/ElfoNoturnoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturnoTest.java
@@ -4,6 +4,56 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ElfoNoturnoTest {
-//TO-DO: ajustar os testes.
+
+    @Test
+    public void elfoNoturnoAtacaUmaVez(){
+        //Arrange
+        Dwarf dwarf = new Dwarf();
+        ElfoNoturno elfo = new ElfoNoturno("Black Legolas");
+        //Act
+        elfo.atirarFlecha(dwarf);
+        //Assert
+        assertEquals(3, elfo.getExperiencia());
+        assertEquals(95.0, elfo.getVida(), .0);
+    }
     
+    
+    @Test
+    public void elfoNoturnoAtacaDuasVezes(){
+        //Arrange
+        Dwarf dwarf = new Dwarf();
+        ElfoNoturno elfo = new ElfoNoturno("Black Legolas");
+        //Act
+        elfo.atirarFlecha(dwarf);
+        elfo.atirarFlecha(dwarf);
+        //Assert
+        assertEquals(6, elfo.getExperiencia());
+        assertEquals(90.25, elfo.getVida(), .0);
+    }
+    
+    @Test
+    public void elfoNoturnoNaoAtaca(){
+        //Arrange
+        Dwarf dwarf = new Dwarf();
+        ElfoNoturno elfo = new ElfoNoturno("Black Legolas");
+        //Act & Assert
+        assertEquals(0, elfo.getExperiencia());
+        assertEquals(100.0, elfo.getVida(), .0);
+    }
+    
+    @Test
+    public void elfoNoturnoAtaca90Vezes(){
+        //Arrange
+        Dwarf dwarf = new Dwarf();
+        ElfoNoturno elfo = new ElfoNoturno("Black Legolas", 150);
+        //Act
+        int auxiliar = 90;
+        while(auxiliar != 0){
+        elfo.atirarFlecha(dwarf);    
+        auxiliar--;
+        }    
+        //Assert
+        assertEquals(270, elfo.getExperiencia());
+        assertEquals(Status.MORTO, elfo.getStatus());
+    }
 }
