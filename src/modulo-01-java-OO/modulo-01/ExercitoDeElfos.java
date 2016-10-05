cf713f9228c5f61743310b8055a1010954ce5ed4
Unknown <mateusbpt1@gmail.com>
2016-10-05 11:13:06 -0300

    feat(ExercitoDeElfos): adicionar classe ExercitoDeElfos e métodos solicitados.

---
+++ src/modulo-01-java-OO/modulo-01/ExercitoDeElfos.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class ExercitoDeElfos {
+
+    ArrayList<Elfo> exercito = new ArrayList<>();    
+
+    public void alistarSoldado(Elfo soldado){
+        boolean tipoSoldadoPossivel = soldado instanceof ElfoVerde || soldado instanceof ElfoNoturno;
+        if(tipoSoldadoPossivel){
+            exercito.add(soldado);
+        }
+    }
+
+    public Elfo buscarPeloNome(String nome){
+        boolean exercitoVazio = exercito.size() == 0;
+        if(!exercitoVazio){
+            for(Elfo elfoAtual : exercito){
+                if(nome.equals(elfoAtual.getNome())){   
+                    return elfoAtual;
+                }
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Elfo> buscarPeloStatus (Status status){
+        boolean exercitoVazio = exercito.size() == 0;
+        ArrayList<Elfo> retorno = new ArrayList<>();
+        if(!exercitoVazio){
+            for(Elfo elfoAtual : exercito){
+                if(status.equals(elfoAtual.getStatus())){   
+                    retorno.add(elfoAtual);    
+                }        
+            }
+            return retorno;
+        }
+        return null;
+    }
+}
