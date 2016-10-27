bf27a738d5f916a7bce47b1ae648c3fd6d535a3f
Unknown <mateusbpt1@gmail.com>
2016-10-27 18:12:43 -0200

    feat(.Net): adicionar exercicios;

---
+++ modulos/modulo-05-.Net/MarioKart/MarioKart/Corredores/Corredor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Corredores
+{
+    public class Corredor
+    {
+        public Corredor(String nome, NivelHabilidade nivel)
+        {
+            this.NomeCorredor = nome;
+            this.HabilidadeCorredor = nivel;
+        }
+
+        protected String NomeCorredor { get; set; }
+
+        public NivelHabilidade HabilidadeCorredor { get; set; }
+
+    }
+
+    public enum NivelHabilidade
+    {
+        Noob, Mediano, Profissional
+    }
+}
+
---
+++ modulos/modulo-05-.Net/MarioKart/MarioKart/Kart/Kart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioKart.Corredores;
+using MarioKart.Equipamentos;
+
+namespace MarioKart.Kart
+{
+    public class Kart
+    {
+        public Kart(Corredor motorista)
+        {
+            this.Motorista = motorista;
+            this.Equipamentos = new List<IEquipamento>();
+        }
+
+        private Corredor Motorista { get; set; }
+
+        protected List<IEquipamento> Equipamentos { get; set; }
+
+        private int Velocidade
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+
+}
