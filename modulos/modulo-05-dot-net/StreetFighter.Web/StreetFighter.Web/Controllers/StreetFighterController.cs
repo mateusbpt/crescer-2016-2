91909516ba813a570549dc0bf6ce3ac91a8c53ad
Unknown <mateusbpt1@gmail.com>
2016-10-31 17:20:40 -0200

    feat(dot-net): adicionar exercicio mvc.

---
+++ modulos/modulo-05-dot-net/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
