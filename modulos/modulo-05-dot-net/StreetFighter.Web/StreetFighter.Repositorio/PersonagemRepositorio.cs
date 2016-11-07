d88b8b8c7f513c4f1a7afb8ddf7ebd67b3a843f4
Unknown <mateusbpt1@gmail.com>
2016-11-07 17:58:33 -0200

    feat(dot-net): adicionar exemplo sql para mvc.

---
+++ modulos/modulo-05-dot-net/StreetFighter.Web/StreetFighter.Repositorio/PersonagemRepositorio.cs
@@ -0,0 +1,207 @@
+﻿using StreetFighter.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Repositorio
+{
+    public class PersonagemRepositorio
+    {
+        const string CaminhoDoArquivo = @"C:\Users\mateus.teixeira\Documents\github\crescer-2016-2\modulos\modulo-05-dot-net\CadastroPersonagens.txt";
+
+        public List<Personagem> ListarPersonagens(string filtro = null)
+        {
+            List<Personagem> listaPersonagens = new List<Personagem>();
+            string[] linhas = File.ReadAllLines(CaminhoDoArquivo);
+            bool listaDePersonagensVazia = linhas == null || linhas.Length == 0;
+
+            if (!listaDePersonagensVazia)
+            {
+                for (int i = 0; i < linhas.Length; i++)
+                {
+                    string[] auxiliar = linhas[i].Split(';');
+
+                    listaPersonagens.Add(new Personagem
+                        (
+                        Convert.ToInt32(auxiliar[0]),
+                        auxiliar[1],
+                        auxiliar[2],
+                        Convert.ToDateTime(auxiliar[3]),
+                        Convert.ToInt32(auxiliar[4]),
+                        Convert.ToDecimal(auxiliar[5]),
+                        auxiliar[6],
+                        Convert.ToBoolean(auxiliar[7]),
+                        auxiliar[8]
+                        ));
+                }
+            }
+
+            return filtro != null ?
+                listaPersonagens.Where(personagem => personagem.Nome.ToUpperInvariant()
+                .Contains(filtro.ToUpperInvariant())).ToList()
+                : listaPersonagens;
+        }
+
+        public void IncluirPersonagem(Personagem personagem)
+        {
+            string novoPersonagem = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};",
+                VerificaUltimoIdUtilizado() + 1,
+                personagem.Nome,
+                personagem.Origem,
+                personagem.DataNascimento,
+                personagem.Altura,
+                personagem.Peso,
+                personagem.GolpesEspeciais,
+                personagem.PersonagemOculto,
+                personagem.Imagem
+                );
+            if (ListarPersonagens().Count == 0)
+            {
+                File.AppendAllText(CaminhoDoArquivo, novoPersonagem);
+            }
+            else
+            {
+                File.AppendAllText(CaminhoDoArquivo, Environment.NewLine + novoPersonagem);
+            }
+
+        }
+
+        public void ExcluirPersonagem(Personagem personagem)
+        {
+            var auxiliar = ListarPersonagens().Where(per => per.Id != personagem.Id).OrderBy(id => id.Id);
+
+            File.WriteAllText(CaminhoDoArquivo, string.Empty);
+
+            foreach (var per in auxiliar)
+            {
+                IncluirPersonagem(per);
+            }
+
+        }
+
+        public void EditarPersonagem(Personagem personagem)
+        {
+            var auxiliar = ListarPersonagens();
+
+            File.WriteAllText(CaminhoDoArquivo, string.Empty);
+
+            for (int i = 0; i < auxiliar.Count; i++)
+            {
+                if (auxiliar[i].Id == personagem.Id)
+                {
+                    auxiliar.RemoveAt(i);
+                    auxiliar.Add(personagem);
+                }
+            }
+
+            auxiliar.OrderBy(per => per.Id);
+
+            foreach (var per in auxiliar)
+            {
+                IncluirPersonagem(per);
+            }
+
+        }
+
+        public int VerificaUltimoIdUtilizado()
+        {
+            var retorno = 0;
+
+            if (ListarPersonagens().Count != 0)
+            {
+                var ultimoPersonagem = ListarPersonagens().OrderBy(id => id.Id).Last();
+                retorno = ultimoPersonagem.Id;
+            }
+
+            return retorno;
+        }
+
+
+
+
+        public List<Personagem> ListarPersonagensSql(string filtro = null)
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["StreetFighter"].ConnectionString;
+            List<Personagem> retorno = new List<Personagem>();
+
+            using (var connection = new SqlConnection(conexao))
+            {
+                connection.Open();
+
+                string sql = $"SELECT * FROM Personagem WHERE Nome LIKE @param_nome";
+
+                var comando = new SqlCommand(sql, connection);
+
+                comando.Parameters.Add(new SqlParameter("param_nome", $"%{filtro}%"));
+
+                SqlDataReader leitura = comando.ExecuteReader();
+
+                while (leitura.Read())
+                {
+                    Personagem personagem = ConverteParaPersonagem(leitura);
+                    retorno.Add(personagem);
+                }
+
+                connection.Close();
+            }
+
+            return retorno;
+        }
+
+        private Personagem ConverteParaPersonagem(SqlDataReader leitura)
+        {
+            Personagem retorno = new Personagem
+                (
+                id: Convert.ToInt32(leitura["id"]),
+                nome: leitura["nome"].ToString(),
+                origem: leitura["origem"].ToString(),
+                dataNascimento: Convert.ToDateTime(leitura["dataNascimento"]),
+                altura: Convert.ToInt32(leitura["altura"]),
+                peso: Convert.ToDecimal(leitura["peso"]),
+                golpesEspeciais: leitura["golpesEspeciais"].ToString(),
+                personagemOculto: Convert.ToBoolean(leitura["personagemOculto"]),
+                imagem: leitura["imagem"].ToString()
+                );
+
+            return retorno;
+        }
+
+        public Personagem PesquisarPorIDSql(int id)
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["StreetFighter"].ConnectionString;
+            Personagem personagem = null;
+
+            using (var connection = new SqlConnection(conexao))
+            {
+                connection.Open();
+
+                string sql = $"SELECT * FROM Personagem WHERE Id = @param_id";
+
+                var comando = new SqlCommand(sql, connection);
+
+                comando.Parameters.Add(new SqlParameter("param_id", id));
+
+                SqlDataReader leitura = comando.ExecuteReader();
+
+                if (leitura.Read())
+                {
+                    personagem = ConverteParaPersonagem(leitura);
+
+                }
+
+                connection.Close();
+            }
+
+            return personagem;
+        }
+
+
+
+    }
+
+}
